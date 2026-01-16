953a96aaf8138805fd8cf2a05fa12337e7c732ef
Maroua <marwaelidrissi24@gmail.com>
2026-01-16 23:51:53 +0100

    Fix: move EDA code to correct file

---
+++ preprocessing/eda.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv("data/raw/sales.csv")
+print(df.head())  # pour vérifier que ça marche
--- preprocessing/preprocessing.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("data/raw/sales.csv")
-print(df.head())  # pour vérifier que ça marche
